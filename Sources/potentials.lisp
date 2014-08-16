f751fcc9db630e66256dbe6c761a2515e9edb66b
Thomas Rogge-Solti <thomas@solti.de>
2014-08-16 15:51:32 +0200

    code cleanup

--- Sources/potentials.lisp
+++ Sources/potentials.lisp
@@ -24,70 +24,6 @@
   (ra-variables))
 
 
-;(defgeneric scope-permutations (scope) 
-;  (:documentation "get all possible permutations for the given scope")
-;  (:method (scope)
-;    (let ((variables (scope-ra-variables))
-;          (variables-length (length variables)))
-;      (dotimes (i variables-length)
-;        (
-;      )))
-
-
-;(defun find-permutations (lst i)
-;  (cond ((= i (length lst)) (format t "~S~%" lst))
-;        (t (let ((j i))
-;	     (dotimes (j (length lst))
-;               (rotatef (nth i lst) (nth j lst))
-;	       (find-permutations lst (+ i 1))
-;               (rotatef (nth i lst) (nth j lst)) ; cleanup return to previous condition
-;)))))
-
-
-;(defgeneric permutations (scope)
-;  (:documentation "renders all permutations of the values of variables in the scope.")
-;  (:method (scope)
-;    (let ((variables (scope-ra-variables scope)))
-;      (dotimes (i (length variables)) ; variables contains relationalAlgebra variables *A* e.g.
-;        (let ((var-at-i (elt variables i))) ; var-at-i = *A*
-;          ;(coerce var-at-i 'ra-variable) ; casting variable to variable, so that we can call object-specific functions 
-;          (let ((values-at-i (ra-variable-domain var-at-i))) 
-;            (dotimes (j (length values-at-i))
-;              (let ((k (+ 1 j)))
-;                (dotimes (k (- length values-at-i 1))
-;                  (format t "~S: ~S~%" (ra-variable-name var-at-i) (elt values-at-i j))
-;            )
-;          )
-;        )
-;      )
-;    )
-;  )
-;)
-
-;(defgeneric permutations (scope)
-;  (:documentation "renders all permutations of the values of variables in the scope.")
-;  (:method (scope)
-;    (let ((variables (scope-ra-variables scope)))
-;      (dotimes (i (length variables)) ; variables contains relationalAlgebra variables *A* e.g.
-;        (let ((var-at-i (elt variables i))) ; var-at-i = *A*
-;          ;(coerce var-at-i 'ra-variable) ; casting variable to variable, so that we can call object-specific functions 
-;          (let ((values-at-i (ra-variable-domain var-at-i))) 
-;            (dotimes (j (length values-at-i))
-;                (format t "~S= ~S~%" (ra-variable-name var-at-i) (elt values-at-i j))
-;                (nconc (ra-variable-named-domain-list var-at-i)  ;must use nconc, so we have call-by-reference effect
-;                       (format nil "~a=~a" (ra-variable-name var-at-i) (elt values-at-i j)) 
-;                )
-;            )
-;          )
-;        )
-;      )
-;    )
-;  )
-;)
-; (setf *s* (nconc *s* (list (format nil ...))))  nasty!!!
-; push!!!
-
-
 (defgeneric permutations (scope)
   (:documentation "renders all permutations of the values of variables in the scope.")
   (:method (scope)
@@ -108,7 +44,7 @@
         (push (ra-variable-named-domain-list (elt variables i)) input-list))
       (multiple-value-call #'combinations (values-list input-list))))) ;using multiple results by applying combinations function via multiple-value-call function
 
-; works only if input lists are not empty
+; FIXME: works only if input lists are not empty
 ; returns all combinations of the list elements 
 (defun combinations (&rest lists)
   (if (car lists)
@@ -146,10 +82,10 @@
 
 
 ;; example-usage:
- (defparameter *A* (make-instance 'ra-variable :name "A" :domain '(1 2 3 4) ))
- (defparameter *B* (make-instance 'ra-variable :name "B" :domain '(1 2) ))
- (defparameter *C* (make-instance 'ra-variable :name "C" :domain '(1 2 3) ))
- (defparameter *scope* (make-instance 'scope :ra-variables (list *A* *B* *C*))) ;; when doing this :ra-variables '(*A* *B* *C*)
+; (defparameter *A* (make-instance 'ra-variable :name "A" :domain '(1 2 3 4) ))
+; (defparameter *B* (make-instance 'ra-variable :name "B" :domain '(1 2) ))
+; (defparameter *C* (make-instance 'ra-variable :name "C" :domain '(1 2 3) ))
+; (defparameter *scope* (make-instance 'scope :ra-variables (list *A* *B* *C*))) ;; when doing this :ra-variables '(*A* *B* *C*)
 ; (permutations *scope*)
 
 ;(defgeneric tuple (scope row-index)
