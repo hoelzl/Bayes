6600d5c9decca24230f4a4dfefde6ba6f2c0b418
Thomas Rogge-Solti <roggesolti@gmail.com>
2014-08-18 16:59:14 +0200

    fixed combinations test with loop and added permutations-test

--- Sources/Tests/potential-tests.lisp
+++ Sources/Tests/potential-tests.lisp
@@ -9,27 +9,33 @@
 
 (in-suite bayes-potentials-suite)
 
+(defun set-exclusive-or-for-strings (list-a list-b)
+  (set-exclusive-or list-a list-b :test #'equal))
+
 (deftest test-combinations ()
   (let ((c1 (combinations '(1 2 3) '(4 5)))
         (c2 (combinations '(4 5) '(1 2 3))))
     (is (equal 6 (length c1)))
     (is (equal 6 (length c2)))
-    (is (member (first c1) c2 :test-not #'set-exclusive-or)) ;todo: replace by loop over c1
-    (is (member (second c1) c2 :test-not #'set-exclusive-or))
- ))
+    (dotimes (i (length c1)) 
+      (is (member (nth i c1) c2 :test-not #'set-exclusive-or)))))
 
-;(defun unordered-list-equals-p (list-a list-b)
-;  (let ((sorted-a (sort list-a #'> :key #'car))
-;        (sorted-b (sort list-b #'> :key #'car)))
-;    (equal sorted-a sorted-b)))
-
-;(defun set-member (set set-list)
-;  (loop for set-list-set in set-list
-;        when (loop for set-item in set
-;                   for item = (if (member set-item set-list-set)
-;                                (remove set-item set-list-set)
-;                                (return nil))
-;                            then (if (member set-item item)
-;                                   (remove set-item item)
-;                                   (return nil))
-;                   finally (unless item (return set)) return it)))
+(deftest test-permutations ()
+  (let ((var-a (make-instance 'ra-variable :name "A" :domain '(1 2 3 4)))
+        (var-b (make-instance 'ra-variable :name "B" :domain '(1 2)))
+        (var-c (make-instance 'ra-variable :name "C" :domain '(1 2 3))))
+    (let ((scope (make-instance 'scope :ra-variables (list var-a var-b var-c))))
+      (let ((perms (permutations scope)))
+        (is (equal 24 (length perms)))
+        (is (member '("A=1" "B=1" "C=1") perms :test-not #'set-exclusive-or-for-strings)) ; testing
+        (is (member '("A=1" "B=1" "C=2") perms :test-not #'set-exclusive-or-for-strings)) ; exemplary
+        (is (member '("A=1" "B=1" "C=3") perms :test-not #'set-exclusive-or-for-strings)) ; cases
+        (is (member '("A=1" "B=2" "C=1") perms :test-not #'set-exclusive-or-for-strings)) ; that
+        (is (member '("A=1" "B=2" "C=2") perms :test-not #'set-exclusive-or-for-strings)) ; belong
+        (is (member '("A=1" "B=2" "C=3") perms :test-not #'set-exclusive-or-for-strings)) ; to
+        (is (member '("A=2" "B=1" "C=2") perms :test-not #'set-exclusive-or-for-strings)) ; the
+        (is (member '("A=3" "B=1" "C=3") perms :test-not #'set-exclusive-or-for-strings)) ; allowed
+        (is (member '("A=4" "B=2" "C=3") perms :test-not #'set-exclusive-or-for-strings)) ;permutations
+        (is (not (member '("A=1" "B=3" "C=1") perms :test-not #'set-exclusive-or-for-strings))) ; also
+        (is (not (member '("A=1" "B=2" "C=4") perms :test-not #'set-exclusive-or-for-strings))) ; not
+        (is (not (member '("A=4" "B=3" "C=4") perms :test-not #'set-exclusive-or-for-strings))))))) ; allowed
