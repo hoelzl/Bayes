4991174a3a8c214dd61091e6284ca81917234eca
Thomas Rogge-Solti <roggesolti@gmail.com>
2014-09-01 19:04:20 +0200

    fixed tests according to new way of node creation

--- Sources/Tests/node-tests.lisp
+++ Sources/Tests/node-tests.lisp
@@ -10,68 +10,70 @@
 (in-suite bayes-nodes-suite)
 
 (deftest test-discrete-node-type-1 ()
-  (let ((n1 (make-discrete-node :values '(a b c)))
-        (n2 (make-discrete-node :values #(a b c))))
+  (let ((n1 (make-discrete-node :domain-values '(a b c) :name "n1" :potential '(0.1 0.2 0.7)))
+        (n2 (make-discrete-node :domain-values #(a b c) :name "n2" :potential '(0.4 0.2 0.4))))
     (is (equal (node-type n1) '(member a b c)))
     (is (equal (node-type n2) '(member a b c)))
 ))
 
 (deftest test-bayesian-network-darwiche-page-127 ()
   (let ((node-a (make-discrete-node 
-                 :values (list "A=t" "A=f") 
+                 :domain-values (list "t" "f") 
                  :kind :nature 
                  :potential (make-array '(2) :initial-contents '(0.6 0.4)) 
-                 :name "A: Winter?")))
-    (is (equal 0.6 (node-probability node-a "A=t")))
-    (is (equal 0.4 (node-probability node-a "A=f")))
+                 :name "A")))
+    (is (equal 0.6 (node-probability node-a (list "A=t"))))
+    (is (equal 0.4 (node-probability node-a (list "A=f"))))
     (let ((node-b (make-discrete-node 
-                   :values (list (list "A=t" "B=t") (list "A=t" "B=f") (list "A=f" "B=t") (list "A=f" "B=f")) 
+                   :domain-values (list "t" "f") 
                    :kind :nature 
                    :potential (make-array '(4) :initial-contents '(0.2 0.8 0.75 0.25)) 
-                   :name "B: Sprinkler?"
+                   :name "B"
                    :parents (list node-a))))
       (is (equal 0.2 (node-probability node-b (list "A=t" "B=t"))))
       (is (equal 0.8 (node-probability node-b (list "A=t" "B=f"))))
       (is (equal 0.75 (node-probability node-b (list "A=f" "B=t"))))
       (is (equal 0.25 (node-probability node-b (list "A=f" "B=f"))))
       (is (member node-a (node-parents node-b))) ; check if a is parent of b
-    ; (is (equal 0.25 (node-probability node-b (list "B=f" "A=f")))) ;TODO: make this work too (commutative)
+      ;; TODO: make this work too (commutative)
+      ;; (is (equal 0.25 (node-probability node-b (list "B=f" "A=f"))))
       (let ((node-c (make-discrete-node 
-                   :values (list (list "A=t" "C=t") (list "A=t" "C=f") (list "A=f" "C=t") (list "A=f" "C=f")) 
-                   :kind :nature 
-                   :potential (make-array '(4) :initial-contents '(0.8 0.2 0.1 0.9)) 
-                   :name "C: Rain?"
-                   :parents (list node-a))))
+                     :domain-values (list "t" "f") 
+                     :kind :nature 
+                     :potential (make-array '(4) :initial-contents '(0.8 0.2 0.1 0.9)) 
+                     :name "C"
+                     :parents (list node-a))))
         (is (member node-a (node-parents node-c))) ; check if a is parent of c
         (let ((node-d (make-discrete-node 
-                   :values (list (list "B=t" "C=t" "D=t") 
-                                 (list "B=t" "C=t" "D=f") 
-                                 (list "B=t" "C=f" "D=t") 
-                                 (list "B=t" "C=f" "D=f")
-                                 (list "B=f" "C=t" "D=t") 
-                                 (list "B=f" "C=t" "D=f") 
-                                 (list "B=f" "C=f" "D=t") 
-                                 (list "B=f" "C=f" "D=f")) 
-                   :kind :nature 
-                   :potential (make-array '(8) :initial-contents '(0.95 0.05 0.9 0.1 0.8 0.2 0 1)) 
-                   :name "D: Wet Grass?"
-                   :parents (list node-b node-c))))
+                       :domain-values (list "t" "f")  
+                       :kind :nature 
+                       :potential (make-array '(8) :initial-contents '(0.95 0.05 0.9 0.1 0.8 0.2 0 1)) 
+                       :name "D"
+                       :parents (list node-b node-c))))
           (is (member node-b (node-parents node-d)))
           (is (member node-c (node-parents node-d)))
+          (is (equal 0.95 (node-probability node-d (list "B=t" "C=t" "D=t"))))
+          (is (equal 0.05 (node-probability node-d (list "B=t" "C=t" "D=f"))))
+          (is (equal 0.9 (node-probability node-d (list "B=t" "C=f" "D=t"))))
+          (is (equal 0.1 (node-probability node-d (list "B=t" "C=f" "D=f"))))
+          (is (equal 0.8 (node-probability node-d (list "B=f" "C=t" "D=t"))))
+          (is (equal 0.2 (node-probability node-d (list "B=f" "C=t" "D=f"))))
+          (is (equal 0 (node-probability node-d (list "B=f" "C=f" "D=t"))))
+          (is (equal 1 (node-probability node-d (list "B=f" "C=f" "D=f"))))
+          
           (let ((node-e (make-discrete-node 
-                   :values (list (list "C=t" "E=t") 
-                                 (list "C=t" "E=f") 
-                                 (list "C=f" "E=t") 
-                                 (list "C=f" "E=f"))
-                   :kind :nature 
-                   :potential (make-array '(4) :initial-contents '(0.7 0.3 0 1)) 
-                   :name "E: Slippery Road?"
-                   :parents (list node-c))))
+                         :domain-values (list "t" "f") 
+                         :kind :nature 
+                         :potential (make-array '(4) :initial-contents '(0.7 0.3 0 1)) 
+                         :name "E"
+                         :parents (list node-c))))
             (is (member node-c (node-parents node-e)))
             (is (member node-a (node-parents (first (node-parents node-e)))))
             (is (equal (node-potential-dimensions node-a) '(2)))
             (is (equal (node-potential-dimensions node-b) '(2 2)))
-))))))
+            (is (equal (node-potential-dimensions node-c) '(2 2)))
+            (is (equal (node-potential-dimensions node-d) '(2 2 2)))
+            (is (equal (node-potential-dimensions node-e) '(2 2)))))))))
 
 
 
